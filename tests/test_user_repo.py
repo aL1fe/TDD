e3731a30dcc679f58cfed757378e829c1cfea6b9
oleksii.smirnov <osmirnov@deloitte.com>
2026-01-12 10:07:26 +0200

    mock service in the users api tests

--- tests/test_user_repo.py
+++ tests/test_user_repo.py
@@ -7,17 +7,13 @@ from app.api_exceptions import NotFoundException
 @pytest.mark.anyio
 async def test_get_user_by_id_success():
     repo = UserRepo()
-
     user = await repo.get_user_by_id(1)
-
     assert user == {"id": 1, "name": "Alice"}
 
 
 @pytest.mark.anyio
 async def test_get_user_by_id_not_found():
     repo = UserRepo()
-
     with pytest.raises(NotFoundException) as exc:
         await repo.get_user_by_id(999)
-
     assert "User with Id '999' not found" in str(exc.value.detail)
--- tests/test_users_api.py
+++ tests/test_users_api.py
@@ -1,35 +1,33 @@
 from fastapi.testclient import TestClient
 from unittest.mock import AsyncMock
 
+from app.api_exceptions import NotFoundException
 from app.main import app
 
 
 client = TestClient(app)
 
 
-def test_get_user_by_id_success():
-    response = client.get("/users/1")
-
-    assert response.status_code == 200
-    assert response.json() == {
-        "id": 1,
-        "name": "Alice"
-    }
-
-
 def test_get_user_endpoint(monkeypatch):
-    mock_service = AsyncMock()
-    mock_service.get_user_by_id.return_value = {"id": 1, "name": "Alice"}
+    user_id = 1
+    user_service = AsyncMock()
+    user_service.get_user_by_id.return_value = {"id": user_id, "name": "Alice"}
 
-    # replacing the service factory with our mock
-    monkeypatch.setattr("app.main.get_user_service", lambda: mock_service)
+    # replacing the service factory from Depends with our mock
+    monkeypatch.setattr("app.main.get_user_service", lambda: user_service)
 
-    response = client.get("/users/1")
+    response = client.get(f"/users/{user_id}")
     assert response.status_code == 200
-    assert response.json() == {"id": 1, "name": "Alice"}
+    assert response.json() == {"id": user_id, "name": "Alice"}
 
 
-def test_get_user_by_id_not_found(): 
-    response = client.get("/users/999")  # non-existent user
+def test_get_user_by_id_not_found(monkeypatch):
+    user_id = 999  # non-existent user
+    user_service = AsyncMock()
+    user_service.side_effect = NotFoundException(detail=f"User with Id '{user_id}' not found")
+
+    monkeypatch.setattr("app.main.get_user_service", lambda: user_service)
+
+    response = client.get(f"/users/{user_id}")
     assert response.status_code == 404 
-    assert response.json() == { "detail": "User with Id '999' not found", 'extra': {}, 'error_code': 'not_found' }
+    assert response.json() == { "detail": f"User with Id '{user_id}' not found", 'extra': {}, 'error_code': 'not_found' }
